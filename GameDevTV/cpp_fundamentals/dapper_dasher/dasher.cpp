c632c90ddc9db6ec4a3d37f1303ef76eac060ba0
Kheavin <kheavin.chap@gmail.com>
2022-12-13 21:45:54 +1300

    [feat] - Added moving nebular and animated it

--- GameDevTV/cpp_fundamentals/dapper_dasher/dasher.cpp
+++ GameDevTV/cpp_fundamentals/dapper_dasher/dasher.cpp
@@ -7,7 +7,18 @@ int main(void)
 
     InitWindow(window_width, window_height, "Dasher");
 
-    const int gravity = 1'000;  // Pixels per s per s
+    const int gravity = 1'200;  // Pixels per s per s
+
+    Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
+    Rectangle nebulaRec;
+    nebulaRec.width = nebula.width / 8;
+    nebulaRec.height = nebula.height / 8;
+    nebulaRec.x = 0;
+    nebulaRec.y = 0;
+    Vector2 nebularPos;
+    nebularPos.x = window_width;
+    nebularPos.y = window_height - nebulaRec.height;
+
 
     Texture2D scarfy = LoadTexture("textures/scarfy.png");
     Rectangle scarfyRec;
@@ -19,13 +30,19 @@ int main(void)
     scarfyPos.x = window_width/2 - scarfyRec.width/2;
     scarfyPos.y = window_height - scarfyRec.height;
 
+    int nebularVelocity = -200;
+
     int velocity = 0;
     int jump_val = -600;    // Pixel per second
+
+    int nebFrame;
+    const float nebUpdateTime = 1.0 / 12.0;
+    float nebRunningTime;
 
     bool isInAir;
 
     int frame;
-    const float updateTime = 1.0 / 12.0;
+    const float updateTime = 1.0 / 16.0;
     float runningTime;
 
     SetTargetFPS(60);
@@ -51,24 +68,41 @@ int main(void)
             velocity += jump_val;
         }
 
+        nebularPos.x += nebularVelocity * dT;
         scarfyPos.y += velocity * dT;
 
         scarfyRec.x = frame * scarfyRec.width;
-        runningTime += dT;
-
-        if (runningTime >= updateTime)
+        if (!isInAir)
         {
-            runningTime = 0.0;
-            frame++;
-            if (frame > 5)
+            runningTime += dT;
+            if (runningTime >= updateTime)
+            {
+                runningTime = 0.0;
+                frame++;
+                if (frame > 5)
+                {
+                    frame = 0;
+                }
+            }
+        }
+
+        nebRunningTime += dT;
+        nebulaRec.x = frame * nebulaRec.width;
+        if (nebRunningTime >= nebUpdateTime)
+        {
+            nebRunningTime = 0.0;
+            nebFrame++;
+            if (frame > 7)
             {
                 frame = 0;
             }
         }
 
+        DrawTextureRec(nebula, nebulaRec, nebularPos, WHITE);
         DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
         EndDrawing();
     }
     UnloadTexture(scarfy);
+    UnloadTexture(nebula);
     CloseWindow();
 }
